4e878da6130acce19de89a3dfcae91cf44c86be6
robrecht scheepers <r.scheepers@kruss.de>
2018-10-11 10:27:49 +0200

    Added empty WPF app with basic MVVM setup

---
+++ WMM/WMM.WPF/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WMM.Data;
+
+namespace WMM.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private async void App_OnStartup(object sender, StartupEventArgs e)
+        {
+            var mainViewModel = new MainViewModel(new DummyRepository());
+            var mainWindow = new MainWindow {DataContext = mainViewModel};
+            mainWindow.Show();
+            await mainViewModel.Initialize();
+        }
+
+        private void App_OnExit(object sender, ExitEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ WMM/WMM.WPF/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMM.Data;
+using WMM.WPF.MVVM;
+
+namespace WMM.WPF
+{
+    public class MainViewModel : ObservableObject
+    {
+        private IRepository _reposistory;
+
+        public MainViewModel(IRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
