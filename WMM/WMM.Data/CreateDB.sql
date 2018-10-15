4a933f9f69df662f8202d730589491897b2d6b2f
robrecht scheepers <r.scheepers@kruss.de>
2018-10-15 10:22:54 +0200

    DB change: added an index and allowed Date null

--- WMM/WMM.Data/CreateDB.sql
+++ WMM/WMM.Data/CreateDB.sql
@@ -2,7 +2,7 @@ BEGIN TRANSACTION;
 DROP TABLE IF EXISTS `Transactions`;
 CREATE TABLE IF NOT EXISTS `Transactions` (
 	`Id`	BLOB NOT NULL,
-	`Date`	TEXT NOT NULL,
+	`Date`	TEXT,
 	`Category`	BLOB NOT NULL,
 	`Amount`	NUMERIC NOT NULL,
 	`Comments`	TEXT,
@@ -11,13 +11,14 @@ CREATE TABLE IF NOT EXISTS `Transactions` (
 	`LastUpdateTime`	TEXT NOT NULL,
 	`LastUpdateAccount`	TEXT NOT NULL,
 	`Deleted`	INTEGER NOT NULL,
-	FOREIGN KEY(`Category`) REFERENCES `Categories`(`Id`)
+	FOREIGN KEY(`Category`) REFERENCES `Categories`(`Id`),
+	PRIMARY KEY(`Id`)
 );
 DROP TABLE IF EXISTS `Categories`;
 CREATE TABLE IF NOT EXISTS `Categories` (
 	`Id`	BLOB NOT NULL,
 	`Area`	BLOB NOT NULL,
-	`Name`	TEXT UNIQUE,
+	`Name`	TEXT NOT NULL UNIQUE,
 	PRIMARY KEY(`Id`),
 	FOREIGN KEY(`Area`) REFERENCES `Areas`(`Id`)
 );
@@ -34,5 +35,9 @@ CREATE INDEX IF NOT EXISTS `TransactionsDeletedIndex` ON `Transactions` (
 DROP INDEX IF EXISTS `TransactionDateIndex`;
 CREATE INDEX IF NOT EXISTS `TransactionDateIndex` ON `Transactions` (
 	`Date`	ASC
+);
+DROP INDEX IF EXISTS `TransactionCategoryIndex`;
+CREATE INDEX IF NOT EXISTS `TransactionCategoryIndex` ON `Transactions` (
+	`Category`
 );
 COMMIT;
