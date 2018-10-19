c8200f55fcdbd4466dcb161accbf28b131f8fd59
robrecht scheepers <r.scheepers@kruss.de>
2018-10-19 13:13:40 +0200

    Style, improve visibility of balance

--- WMM/WMM.WPF/Converters/AmountColorConverter.cs
+++ WMM/WMM.WPF/Converters/AmountColorConverter.cs
@@ -11,18 +11,27 @@ namespace WMM.WPF.Converters
 {
     public class AmountColorConverter : IValueConverter
     {
+        public AmountColorConverter()
+        {
+            PositiveColor = Colors.Green;
+            NegativeColor = Colors.Red;
+        }
+
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var amount = System.Convert.ToDouble(value);
 
             return new SolidColorBrush(amount < 0 
-                ? Colors.DarkRed
-                : Colors.Green);
+                ? NegativeColor
+                : PositiveColor);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
+
+        public Color PositiveColor { get; set; }
+        public Color NegativeColor { get; set; }
     }
 }
