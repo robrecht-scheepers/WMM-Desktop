7524b3b8b40788b70099c1c2b550f51bbc2cda88
robrecht scheepers <r.scheepers@kruss.de>
2019-04-29 12:33:06 +0200

    First localized captions working

--- WMM/WMM.WPF/App.xaml.cs
+++ WMM/WMM.WPF/App.xaml.cs
@@ -15,7 +15,7 @@ namespace WMM.WPF
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            var vCulture = new CultureInfo("de-DE");
+            var vCulture = new CultureInfo("nl-BE");
 
             Thread.CurrentThread.CurrentCulture = vCulture;
             Thread.CurrentThread.CurrentUICulture = vCulture;
