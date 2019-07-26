83d715aae7e0286ce22f8d010e8fca7f3becbeed
robrecht scheepers <r.scheepers@kruss.de>
2019-07-26 13:08:25 +0200

    Extend forecast type

--- WMM/WMM.Data/ForecastType.cs
+++ WMM/WMM.Data/ForecastType.cs
@@ -8,6 +8,7 @@ namespace WMM.Data
     {
         Exception = 0,  // exceptional expenses, not forcastable
         Monthly = 1,    // to be forcasted by a monthly mean value
-        Daily = 2       // to be forcasted by extrapolating a daily mean value 
+        Daily = 2,      // to be forcasted by extrapolating a daily mean value 
+        Recurring = 3   // forecast only based on recurring templates
     }
 }
