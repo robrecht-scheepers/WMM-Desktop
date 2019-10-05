09dba928c828fa4d98aded968e52fe1becae25eb
robrecht scheepers <r.scheepers@kruss.de>
2019-10-05 23:04:05 +0200

    Edit functionality

--- WMM/WMM.WPF/Goals/GoalViewModel.cs
+++ WMM/WMM.WPF/Goals/GoalViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using WMM.Data;
@@ -19,13 +20,16 @@ namespace WMM.WPF.Goals
         private readonly List<Category> _categories;
         private readonly IRepository _repository;
         private readonly IWindowService _windowService;
-        private List<ISelectableItem> _criteria;
+        private List<ISelectableItem> _editedCriteria;
         private string _name;
         private string _description;
         private double _limit;
         private AsyncRelayCommand _saveChangesCommand;
         private RelayCommand _resetCommand;
         private string _criteriaString;
+        private string _editedName;
+        private string _editedDescription;
+        private double _editedLimit;
 
         public GoalViewModel(Goal goal, List<CategoryTypeSelectionItem> categoryTypes, List<string> areas, 
             List<Category> categories, List<ISelectableItem> criteria,
@@ -37,21 +41,28 @@ namespace WMM.WPF.Goals
             _categories = categories;
             _repository = repository;
             _windowService = windowService;
-            Criteria = criteria;
+            EditedCriteria = criteria;
+            InitializeFields();
+        }
 
+        public void InitializeFields()
+        {
             Name = Goal.Name;
+            EditedName = Name;
             Description = Goal.Description;
+            EditedDescription = Description;
             Limit = Goal.Limit;
+            EditedLimit = Limit;
             CriteriaString = CreateCriteriaString(Goal);
             SetSelectedCriteria();
         }
 
         public Goal Goal { get; private set; }
 
-        public List<ISelectableItem> Criteria
+        public List<ISelectableItem> EditedCriteria
         {
-            get => _criteria;
-            set => SetValue(ref _criteria, value);
+            get => _editedCriteria;
+            set => SetValue(ref _editedCriteria, value);
         }
 
         public string Name
@@ -60,10 +71,22 @@ namespace WMM.WPF.Goals
             set => SetValue(ref _name, value);
         }
 
+        public string EditedName
+        {
+            get => _editedName;
+            set => SetValue(ref _editedName, value);
+        }
+
         public string Description
         {
             get => _description;
             set => SetValue(ref _description, value);
+        }
+
+        public string EditedDescription
+        {
+            get => _editedDescription;
+            set => SetValue(ref _editedDescription, value);
         }
 
         public string CriteriaString
@@ -78,29 +101,35 @@ namespace WMM.WPF.Goals
             set => SetValue(ref _limit, value);
         }
 
+        public double EditedLimit
+        {
+            get => _editedLimit;
+            set => SetValue(ref _editedLimit, value);
+        }
+
         private void SetSelectedCriteria()
         {
-            foreach (var criterion in Criteria)
+            foreach (var criterion in EditedCriteria)
             {
                 criterion.IsSelected = false;
             }
             foreach (var categoryType in Goal.CategoryTypeCriteria)
             {
-                Criteria.Cast<AreaCategoryMultiSelectionItem>().First(x =>
+                EditedCriteria.Cast<AreaCategoryMultiSelectionItem>().First(x =>
                     x.Item.SelectionType == AreaCategorySelectionItem.AreaCategorySelectionType.CategoryType &&
                     x.Item.Name == _categoryTypes.First(y => y.CategoryType == categoryType).Caption).IsSelected = true;
             }
 
             foreach (var areaCriterion in Goal.AreaCriteria)
             {
-                Criteria.Cast<AreaCategoryMultiSelectionItem>().First(x =>
+                EditedCriteria.Cast<AreaCategoryMultiSelectionItem>().First(x =>
                     x.Item.SelectionType == AreaCategorySelectionItem.AreaCategorySelectionType.Area &&
                     x.Item.Name == areaCriterion).IsSelected = true;
             }
 
             foreach (var category in Goal.CategoryCriteria)
             {
-                Criteria.Cast<AreaCategoryMultiSelectionItem>().First(x =>
+                EditedCriteria.Cast<AreaCategoryMultiSelectionItem>().First(x =>
                     x.Item.SelectionType == AreaCategorySelectionItem.AreaCategorySelectionType.Category &&
                     x.Item.Name == category.Name).IsSelected = true;
             }
@@ -110,28 +139,26 @@ namespace WMM.WPF.Goals
         
         private async Task SaveChanges()
         {
-            var typeCriteria = Criteria.Cast<AreaCategoryMultiSelectionItem>().Where(x =>
+            var typeCriteria = EditedCriteria.Cast<AreaCategoryMultiSelectionItem>().Where(x =>
                     x.IsSelected && x.Item.SelectionType == AreaCategorySelectionItem.AreaCategorySelectionType.CategoryType)
                 .Select(x => _categoryTypes.First(y => y.Caption == x.Item.Name).CategoryType).ToList();
-            var areaCriteria = Criteria.Cast<AreaCategoryMultiSelectionItem>().Where(x =>
+            var areaCriteria = EditedCriteria.Cast<AreaCategoryMultiSelectionItem>().Where(x =>
                     x.IsSelected && x.Item.SelectionType == AreaCategorySelectionItem.AreaCategorySelectionType.Area)
                 .Select(x => x.Item.Name).ToList();
-            var categoryCriteria = Criteria.Cast<AreaCategoryMultiSelectionItem>().Where(x =>
+            var categoryCriteria = EditedCriteria.Cast<AreaCategoryMultiSelectionItem>().Where(x =>
                     x.IsSelected && x.Item.SelectionType ==
                     AreaCategorySelectionItem.AreaCategorySelectionType.Category)
                 .Select(x => _categories.First(y => y.Name == x.Item.Name)).ToList();
 
-            Goal = await _repository.UpdateGoal(Goal, Name, Description, typeCriteria, areaCriteria, categoryCriteria, Limit);
+            Goal = await _repository.UpdateGoal(Goal, EditedName, EditedDescription, typeCriteria, areaCriteria, categoryCriteria, EditedLimit);
+            InitializeFields();
         }
 
         public RelayCommand ResetCommand => _resetCommand ?? (_resetCommand = new RelayCommand(Reset));
 
         private void Reset()
         {
-            Name = Goal.Name;
-            Description = Goal.Description;
-            Limit = Goal.Limit;
-            SetSelectedCriteria();
+            InitializeFields();
         }
 
         private string CreateCriteriaString(Goal goal)
